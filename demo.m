bdfe355a09d0cb9eb18dd3781539dc88cca5ba14
jovo <joshuav@jhu.edu>
2011-08-27 13:33:56 -0700

    fixed a minor bug and commented demo better

--- demo.m
+++ demo.m
@@ -1,29 +1,30 @@
-clear, clc,
+% clear, clc,
 
 % set simulation metadata
-T               = 1500;     % # of time steps
-V.dt            = 1/200;    % time step size
+T       = 1000; % # of time steps
+V.dt    = 1/8;  % time step size
 
 % initialize params
-P.a     = 200;              % scale
-P.b     = 1/P.a;            % bias
-tau     = 0.5;              % decay time constant for each cell
-P.gam   = 1-V.dt/tau;       % gam
-P.lam   = 2.0;              % rate
-P.sig   = 50;              % noise
+P.a     = 1;    % observation scale
+P.b     = 0;    % observation bias
+tau     = 1;    % decay time constant
+P.gam   = 1-V.dt/tau; % C(t) = gam*C(t-1)
+P.lam   = 0.1;  % firing rate = lam*dt
+P.sig   = 0.1;  % standard deviation of observation noise 
 
 % simulate data
 N = poissrnd(P.lam*V.dt*ones(T,1)); % simulate spike train
 C = filter(1,[1 -P.gam],N);         % calcium concentration
-F = P.a*C+P.b + P.sig*randn(T,1);
+F = P.a*C+P.b + P.sig*randn(T,1);   % observations
 
 % infer spikes
 [Nhat Phat] = fast_oopsi(F,V,P);
 
 % plot results
 figure(1), clf
-h(1)=subplot(311); plot(F); axis('tight')
-h(2)=subplot(312); plot(C);
-h(3)=subplot(313); stem(N);
-hold on, plot(Nhat,'r')
-linkaxes(h,'x')
+tvec=0:V.dt:(T-1)*V.dt;
+h(1)=subplot(311); plot(tvec,F); axis('tight'), ylabel('F (au)')
+h(2)=subplot(312); plot(tvec,C); axis('tight'), ylabel('C (au)')
+h(3)=subplot(313); stem(tvec,N); axis('tight'), ylabel('N (# spikes)')
+hold on, plot(tvec,Nhat,'r'), xlabel('time (sec)') % estimated spike train
+linkaxes(h,'x'), legend('spike train', 'estimate')
